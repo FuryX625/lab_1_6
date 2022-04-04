ee2715a90e8b6e330aec08df223de9e8b203126e
serafin625 <serafin.m625@gmail.com>
2022-04-04 14:38:23 +0000

    poprawiony zły licznik pętli do dzielenia

--- Main.java
+++ Main.java
@@ -1,31 +1,29 @@
 import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    int liczba, ilosc_dzielnikow=0;
-    Scanner odczyt=new Scanner(System.in);
+    int liczba, ilosc_dzielnikow = 0;
+    Scanner odczyt = new Scanner(System.in);
     System.out.println("Podaj liczbe naturalną");
-    liczba=odczyt.nextInt();
-    if (liczba<=0){
+    liczba = odczyt.nextInt();
+    if (liczba <= 0) {
       System.out.println("Złe dane");
       System.exit(0);
-    }
-    else if(liczba==1){
-      System.out.println(liczba+" nie jest liczbą pierwszą");
+    } else if (liczba == 1) {
+      System.out.println(liczba + " nie jest liczbą pierwszą");
       return;
-    }
-    else if(liczba==2){
-      System.out.println(liczba+" jest liczbą pierwszą");
+    } else if (liczba == 2) {
+      System.out.println(liczba + " jest liczbą pierwszą");
       return;
-    }
+    } else
+      for (int i = 1; i <= liczba; i++) {
+        if (liczba % i == 0)
+          ilosc_dzielnikow++;
+      }
+    if (ilosc_dzielnikow == 2)
+      System.out.println(liczba + " jest liczbą pierwszą");
     else
-    for (int i=3;i<=liczba;i++){
-      if (liczba%i==0)
-        ilosc_dzielnikow++;
-    }
-    if(ilosc_dzielnikow==2)
-      System.out.printl(liczba+" jest liczbą pierwszą");
-    else
-      System.out.printl(liczba+" nie jest liczbą pierwszą");
+      System.out.println(liczba + " nie jest liczbą pierwszą");
     return;
   }
 }
